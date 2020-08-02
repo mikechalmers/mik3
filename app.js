8746e7730f72dd6844db92de2a5cff2465ca1ef1
Mike Chalmers <mikechalmers@gmail.com>
2020-08-02 10:29:32 +0100

    Add helmet security

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// security
+var helmet = require('helmet')
+app.use(helmet())
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
